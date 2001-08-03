5e2e8709597ea2ef4fbaaf7892b4da0f9a588fd6
ahelsing <ahelsing>
2001-08-03 18:18:50 +0000

    Update this test of the TaskFileParser so that it works, and doesnt require a separate external data file

--- csmart/regress/org/cougaar/tools/csmart/util/parser/ParserTest.java
+++ csmart/regress/org/cougaar/tools/csmart/util/parser/ParserTest.java
@@ -1,28 +1,75 @@
-package org.cougar.tools.csmart.util.parser;
+/* 
+ * <copyright>
+ * This software is to be used only in accordance with the COUGAAR license
+ * agreement. The license agreement and other information can be found at
+ * http://www.cougaar.org
+ * 
+ *       © Copyright 2001 by BBNT Solutions LLC.
+ * </copyright>
+ */
+package org.cougaar.tools.csmart.util.parser;
 
 import java.io.IOException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 import java.util.Enumeration;
 
 import junit.framework.*;
 
-import com.bbn.abc.ul.ldm.plugin.customer.*;
+import org.cougaar.tools.csmart.ldm.plugin.customer.*;
 
+/**
+ * Test the TaskFileParser in a fairly trivial way.<br>
+ * Note that this writes out a hard-coded test data file.
+ *
+ */
 public class ParserTest extends TestCase {
 
   private TaskFileParser tfp = new TaskFileParser();
   private Enumeration tasks = null;
+  File testFile = null; // handle to test file, to remove when done
 
   public ParserTest(String name) {
      super(name);
   }
     
+  // Write out the test data file, then load it
   protected void setUp() {
-     try {
-        tfp.load("testScript.ini");
-        tfp.parse();
-     } catch(IOException e) {}  
+    try {
+      testFile = new File("testScript.ini");
+    } catch (NullPointerException e) {
+    }
+    try {
+      PrintWriter writer = new PrintWriter(new FileOutputStream(testFile));
+      writer.println("# <WorldState>, <TaskType>, <Rate>, <Deviation>, <Vitality>, <Duration>");
+      writer.println("PEACE, A, 50, 10, 0.2, 100");
+      writer.println("WAR,B,5,25,0.98,2500");
+      writer.println("PEACE,D,45,0,1,20");
+      writer.close();
+    } catch (SecurityException e) {
+      fail("Got exception writing test file: " + e);
+    } catch (FileNotFoundException e) {
+      fail("Got exception writing test file: " + e);
+    }
+
+    try {
+      tfp.load("testScript.ini");
+      tfp.parse();
+    } catch(IOException e) {
+      fail("Got exception loading testcript: " + e);
+    }  
   }
   
+  protected void tearDown() {
+    if (testFile != null) {
+      try {
+	testFile.delete();
+      } catch (SecurityException e) {}
+    }
+  }
+
   public void testParse() {     
      tasks = tfp.elements();
      assert(tasks.hasMoreElements());
