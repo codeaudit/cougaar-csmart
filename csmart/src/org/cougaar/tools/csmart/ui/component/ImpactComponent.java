eb9ea4695d1d77d0fbe97c02672b41fd9e8e02b5
bkrisler <bkrisler>
2001-08-23 13:58:19 +0000

    Impacts as a component.  Initial version

---
+++ csmart/src/org/cougaar/tools/csmart/ui/component/ImpactComponent.java
@@ -0,0 +1,88 @@
+/*
+ * <copyright>
+ *  Copyright 2000-2001 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.tools.csmart.ui.component;
+
+import java.net.URL;
+
+import org.cougaar.tools.csmart.ui.viewer.Organizer;
+import org.cougaar.tools.server.ConfigurationWriter;
+
+/**
+ * Parent interface for all Impacts to a CSMART system.
+ * Impacts may have Agents, and may need to be able to write
+ * out information in the various Nodes. These interfaces are
+ * exposed.
+ */
+public interface ImpactComponent extends ModifiableComponent, ComponentProperties {
+
+  void setName(String newName);
+
+  String getImpactName();
+  
+  /**
+   * Get the agents, both assigned and unassigned.
+   * @return array of agent components
+   */
+  AgentComponent[] getAgents();
+
+  
+  /**
+   * Get Impact description
+   *
+   * @return an <code>URL</code> of the impact description
+   */
+  URL getDescription();
+
+
+  /**
+   * Returns whether or not the impacts can be edited.
+   *
+   * @return true if impacts can be edited and false otherwise
+   */
+  public boolean isEditable();
+  
+  /**
+   * Return a deep copy of the society
+   *
+   * @param organizer an <code>Organizer</code> value
+   * @param context an <code>Object</code> value
+   * @return ImpactComponent created
+   */
+  public ImpactComponent copy(Organizer organizer, Object context);
+
+  /**
+   * Get a configuration writer for this Impact.
+   */
+  public ConfigurationWriter getConfigurationWriter(NodeComponent[] nodes);
+
+  /**
+   * This is the opportunity for an impact to specify additional
+   * components to load into non-Impact Agents
+   *
+   * @return a <code>String</code> Node file addition, possibly null
+   */
+  public String getNodeFileAddition();
+}
+
+
+
+
