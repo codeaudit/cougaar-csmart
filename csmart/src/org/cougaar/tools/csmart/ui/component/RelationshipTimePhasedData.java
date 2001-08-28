8f2187bc7a4f52f7b01fbd76e0b4d16bc3ee6b9c
bkrisler <bkrisler>
2001-08-28 21:51:51 +0000

    Set start and stop string to "" in constructor

--- csmart/src/org/cougaar/tools/csmart/ui/component/RelationshipTimePhasedData.java
+++ csmart/src/org/cougaar/tools/csmart/ui/component/RelationshipTimePhasedData.java
@@ -36,6 +36,8 @@ public class RelationshipTimePhasedData extends TimePhasedData {
 
   public RelationshipTimePhasedData() {
     super();
+    setStartTime("");
+    setStopTime("");
   }
 
   /** 
