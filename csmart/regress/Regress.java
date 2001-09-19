811bc1d9427ee7207d63bd7062ce6f7f0452e3bd
bkrisler <bkrisler>
2001-09-19 21:44:46 +0000

    Removed old test

--- csmart/regress/Regress.java
+++ csmart/regress/Regress.java
@@ -58,7 +58,7 @@ public class Regress extends TestSuite {
     csmartTests.addTest(new TestSuite(org.cougaar.tools.csmart.ui.component.GenericLeafComponentDataTest.class));    
     csmartTests.addTest(new TestSuite(org.cougaar.tools.csmart.ui.component.AgentComponentDataTest.class));    
     csmartTests.addTest(new TestSuite(org.cougaar.tools.csmart.ui.component.CommunityTimePhasedDataTest.class));    
-    csmartTests.addTest(new TestSuite(org.cougaar.tools.csmart.ui.component.RelationshipTimePhasedDataTest.class));    
+
     csmartTests.addTest(new TestSuite(org.cougaar.tools.csmart.ui.component.TimePhasedDataTest.class));    
     csmartTests.addTest(new TestSuite(org.cougaar.tools.csmart.configgen.abcsociety.ABCAgentTest.class));    
 
