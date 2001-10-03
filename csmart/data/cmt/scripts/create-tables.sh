b97d507f1a65181ed1f3333f0376c5cceadfb334
berliner <berliner>
2001-10-03 22:50:24 +0000

    obsolete files

--- csmart/data/cmt/scripts/create-tables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# Load all the V3_ASB, V3_ EXPT, and V3_LIB data in the data directory using the control files in the ctl directory
-#USERID= must be defined as "username/password"
-sqlplus $USERID @sql/create-v3-asb-tables.sql
-
