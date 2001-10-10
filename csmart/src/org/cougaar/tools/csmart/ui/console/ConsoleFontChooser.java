3fe134b572f6e67c6a2d2de6512f3b8ac338d23f
mkennedy <mkennedy>
2001-10-10 17:42:10 +0000

    user can set the font size, type and color for various node events

--- csmart/src/org/cougaar/tools/csmart/ui/console/ConsoleFontChooser.java
+++ csmart/src/org/cougaar/tools/csmart/ui/console/ConsoleFontChooser.java
@@ -23,17 +23,130 @@ package org.cougaar.tools.csmart.ui.console;
 
 import java.awt.Color;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.*;
+import javax.swing.JColorChooser;
 
 import org.cougaar.tools.server.NodeEvent;
 
 public class ConsoleFontChooser extends JDialog {
+  private JComboBox msgTypesCB;
+  private JComboBox fontSizeCB;
+  private JComboBox fontStyleCB;
+  private JButton colorButton;
+  private String SELECTONE = "Select One";
+  private String SEARCH = "Search";
+  private String NOTIFY = "Notify";
+  private static final String STANDARDOUT = "Standard Out";
+  private static final String ERRORMSGS = "Error Messages";
+  private static final String HEARTBEAT = "Heartbeat";
+  private static final String NODECREATION = "Node Creation";
+  private static final String CLUSTERADD = "Cluster Addition";
+  private static final String IDLENESS = "Idleness";
+  private static final String NODEDESTROYED = "Node Destroyed";
+
+
+
+  private static final String FONTSIZE[] = {"8","9","10","11","12","14","16","18","20","22","24","26","28","36","48","72"};
+  private static final String FONTS[] = {"Regular","Italic","Bold","BoldItalic"};
+  int[] fontSizes = new int[8];
+  String[] fontTypes = new String[8];
+  Color[] fontColors = new Color[8];
+  Color selectedColor;
 
   public ConsoleFontChooser() {
     // create a modal dialog
     super((java.awt.Frame)null, "Select Font", true); 
 
-    JPanel panel = new JPanel();
+    JPanel panel = new JPanel(new BorderLayout());
     // define the gui here
+
+    // apply and cancel buttons panel
+    JPanel buttonPanel = new JPanel();
+    JButton applyButton = new JButton("Apply");
+    // when user clicks apply add the selections to the following
+    // arrays - fontSizes, fontTypes, fontColors
+    applyButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        int index = msgTypesCB.getSelectedIndex()-1;
+        if (index <0) return;
+        System.out.println("index is - " + index + "and font style is " + fontStyleCB.getSelectedItem());
+        fontSizes[index]=new Integer((String)fontSizeCB.getSelectedItem()).intValue();
+        fontTypes[index]=(String)fontStyleCB.getSelectedItem();
+        fontColors[index]=selectedColor;
+      }
+    });
+    buttonPanel.add(applyButton);
+    JButton cancelButton = new JButton("Cancel");
+    cancelButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        setVisible(false);
+      }
+    });
+    buttonPanel.add(cancelButton);
+
+
+    // Search Panel
+    JPanel selectPanel = new JPanel();
+    selectPanel.setLayout(new GridBagLayout());
+    TitledBorder selectTitledBorder = new TitledBorder("Select Font");
+    Font selectfont = selectTitledBorder.getTitleFont();
+    Font selecttitleFont = selectfont.deriveFont(Font.ITALIC);
+    selectTitledBorder.setTitleFont(selecttitleFont);
+    selectPanel.setBorder(selectTitledBorder);
+
+    msgTypesCB = new JComboBox();
+    // MEK - easier way to do this?
+    msgTypesCB.addItem(SELECTONE);
+    msgTypesCB.addItem(STANDARDOUT);
+    msgTypesCB.addItem(ERRORMSGS);
+    msgTypesCB.addItem(HEARTBEAT);
+    msgTypesCB.addItem(NODECREATION);
+    msgTypesCB.addItem(CLUSTERADD);
+    msgTypesCB.addItem(IDLENESS);
+    msgTypesCB.addItem(NODEDESTROYED);
+    msgTypesCB.addItem(SEARCH);
+    msgTypesCB.addItem(NOTIFY);
+
+    fontSizeCB = new JComboBox(FONTSIZE);
+    fontSizeCB.setSelectedIndex(4);
+
+    fontStyleCB = new JComboBox(FONTS);
+
+    // Color choser
+    colorButton = new JButton("Font Color");
+    colorButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+      selectedColor = JColorChooser.showDialog(null, "Please Chose a Font Color", null);
+      colorButton.setBackground(selectedColor);
+      }
+    });
+    int x = 0;
+    int y = 0;
+    selectPanel.add(msgTypesCB,
+		   new GridBagConstraints(x, y++, 1, 1, 0.0, 0.0,
+					  GridBagConstraints.WEST,
+					  GridBagConstraints.HORIZONTAL,
+					  new Insets(0, 0, 0, 0), 0, 0));
+    selectPanel.add(fontSizeCB,
+		   new GridBagConstraints(x, y++, 1, 1, 0.0, 0.0,
+					  GridBagConstraints.WEST,
+					  GridBagConstraints.HORIZONTAL,
+					  new Insets(0, 0, 0, 0), 0, 0));
+    selectPanel.add(fontStyleCB,
+		   new GridBagConstraints(x, y++, 1, 1, 0.0, 0.0,
+					  GridBagConstraints.WEST,
+					  GridBagConstraints.HORIZONTAL,
+					  new Insets(0, 0, 0, 0), 0, 0));
+    selectPanel.add(colorButton,
+		   new GridBagConstraints(x, y++, 1, 1, 0.0, 0.0,
+					  GridBagConstraints.WEST,
+					  GridBagConstraints.HORIZONTAL,
+					  new Insets(0, 0, 0, 0), 0, 0));
+
+    panel.add(buttonPanel, BorderLayout.SOUTH);
+    panel.add(selectPanel, BorderLayout.CENTER);
     getContentPane().add(panel);
     pack();
   }
@@ -43,15 +156,21 @@ public class ConsoleFontChooser extends JDialog {
   // plus define two new types in this file (SEARCH and NOTIFY)
 
   public int getFontSize(int messageType) {
-    return 0;
+    int size = fontSizes[messageType];
+    //System.out.println(size);
+    return size;
   }
 
   public String getFontStyle(int messageType) {
-    return "";
+    String style = fontTypes[messageType];
+    //System.out.println(style);
+    return style;
   }
 
   public Color getFontColor(int messageType) {
-    return null;
+    Color fontColor = fontColors[messageType];
+    //System.out.println(fontColor);
+    return fontColor;
   }
 
   // for testing
