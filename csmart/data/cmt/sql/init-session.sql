e6a766ef1cd4d0323695fde69e9f9b0af868d62c
berliner <berliner>
2001-10-18 03:13:16 +0000

    Changed my default output for dates to match mysql standard

--- csmart/data/cmt/sql/init-session.sql
+++ csmart/data/cmt/sql/init-session.sql
@@ -1,4 +1,5 @@
 set tab off;
 set lines 10000;
 set pages 10000;
-alter session set NLS_DATE_FORMAT='DD-MON-YYYY HH24:MI:SS';
+-- alter session set NLS_DATE_FORMAT='DD-MON-YYYY HH24:MI:SS';
+alter session set NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS';
