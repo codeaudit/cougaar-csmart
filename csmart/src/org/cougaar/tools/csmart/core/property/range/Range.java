33ba0d7cc61d653e496d316d1750420406a9665f
bkrisler <bkrisler>
2002-01-09 16:57:28 +0000

    New package for all Range related classes

---
+++ csmart/src/org/cougaar/tools/csmart/core/property/range/Range.java
@@ -0,0 +1,51 @@
+/*
+ * <copyright>
+ *  Copyright 2000-2001 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.tools.csmart.ui.component;
+
+import java.io.Serializable;
+/**
+ * The interface for values that can be get/set on a configurable component.
+ * There are implementations of this for Integer and String.
+ */
+
+public interface Range extends Serializable, Comparable {
+    /**
+     * Get the minimum value of the range. Values are allowed to be
+     * equal to the minimum value
+     * @return an object having the minimum value
+     **/
+    Object getMinimumValue();
+
+    /**
+     * Get the maximum value of the range. Values are allowed to
+     * be equal to the maximum value
+     * @return an object having the maximum value
+     **/
+    Object getMaximumValue();
+
+    /**
+     * Test if an Object is in this Range
+     * @param o the Object to test
+     **/
+    boolean isInRange(Object o);
+}
+
---
+++ csmart/src/org/cougaar/tools/csmart/core/property/range/StringRange.java
@@ -0,0 +1,71 @@
+/*
+ * <copyright>
+ *  Copyright 2000-2001 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.tools.csmart.ui.component;
+
+/**
+ * Implementation of Range for Strings.  Note that a StringRange
+ * always has a single String value.
+ */
+
+public class StringRange extends RangeBase implements Range {
+  static final long serialVersionUID = -7351616054609766392L;
+
+  String value;
+
+  public StringRange(String value) {
+    this.value = value;
+  }
+
+  /**
+   * Get the minimum value of the range. Values are allowed to be
+   * equal to the minimum value
+   * @return an object having the minimum value
+   **/
+
+  public Object getMinimumValue() {
+    return value;
+  }
+
+  /**
+   * Get the maximum value of the range. Values are allowed to
+   * be equal to the maximum value
+   * @return an object having the maximum value
+   **/
+
+  public Object getMaximumValue() {
+    return value;
+  }
+
+  /**
+   * Test if an Object is in this Range
+   * @param o the Object to test
+   **/
+  
+  public boolean isInRange(Object o) {
+    return value.equals(o);
+  }
+
+  public String toString() {
+    return value;
+  }
+}
+
