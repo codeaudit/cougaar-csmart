7ec9d01ef8c3ad5333f81ecb95ae7f94f53436a8
bkrisler <bkrisler>
2002-01-09 17:09:53 +0000

    moved from different pacakges

---
+++ csmart/src/org/cougaar/tools/csmart/recipe/ABCKineticImpact.java
@@ -0,0 +1,160 @@
+/* 
+ * <copyright>
+ *  Copyright 2001 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+package org.cougaar.tools.csmart.ui.experiment;
+
+import org.cougaar.tools.csmart.ui.component.*;
+
+import java.io.Serializable;
+
+public class ABCKineticImpact
+  extends ModifiableConfigurableComponent
+  implements PropertiesListener, Serializable {
+
+
+  private static final String DESCRIPTION_RESOURCE_NAME = "kinetic-impact-description.html";
+  private static final String BACKUP_DESCRIPTION =  "Defines a Kinetic Impact";
+  
+  public static final String PROP_LATITUDE = "Latitude";
+  public static final Double PROP_LATITUDE_DFLT = new Double(0.0);
+  public static final String PROP_LATITUDE_DESC = "Latitude for center of Kinetic Event";
+
+  public static final String PROP_LONGITUDE = "Longitude";
+  public static final Double PROP_LONGITUDE_DFLT = new Double(0.0);
+  public static final String PROP_LONGITUDE_DESC = "Longitude for center of Kinetic Event";
+
+  public static final String PROP_TIME = "Time";
+  public static final String PROP_TIME_DFLT = new String();
+  public static final String PROP_TIME_DESC = "Time cyber attack starts (milliseconds)";
+  
+  public static final String PROP_TYPE = "Type";
+  public static final String PROP_TYPE_DFLT = new String();
+  public static final String PROP_TYPE_DESC = "Type of kinetic attack";
+
+  public static final String PROP_INTENSITY = "Intensity";
+  public static final Double PROP_INTENSITY_DFLT = new Double(0.0);
+  public static final String PROP_INTENSITY_DESC = "Intensity of the attack";
+
+  public static final String PROP_DURATION = "Duration";
+  public static final Long   PROP_DURATION_DFLT = new Long(0);
+  public static final String PROP_DURATION_DESC = "Duration of the attack";
+
+  private Property propLatitude;
+  private Property propLongitude;
+  private Property propTime;
+  private Property propType;
+  private Property propIntensity;
+  private Property propDuration;
+
+  public ABCKineticImpact() {
+    this("Kinetic Event");
+  }
+
+  public ABCKineticImpact(String name) {
+    super(name);
+  }
+
+  public void initProperties() {
+    propLatitude = addProperty(PROP_LATITUDE, PROP_LATITUDE_DFLT, 
+			       new ConfigurableComponentPropertyAdapter() {
+				 public void propertyValueChanged(PropertyEvent e) {
+				 }
+			       });
+    propLatitude.setToolTip(PROP_LATITUDE_DESC);
+
+    propLongitude = addProperty(PROP_LONGITUDE, PROP_LONGITUDE_DFLT, 
+			       new ConfigurableComponentPropertyAdapter() {
+				 public void propertyValueChanged(PropertyEvent e) {
+				 }
+			       });
+    propLongitude.setToolTip(PROP_LONGITUDE_DESC);
+
+    propTime = addProperty(PROP_TIME, PROP_TIME_DFLT, 
+			       new ConfigurableComponentPropertyAdapter() {
+				 public void propertyValueChanged(PropertyEvent e) {
+				 }
+			       });
+    propTime.setToolTip(PROP_TIME_DESC);
+
+    propType = addProperty(PROP_TYPE, PROP_TYPE_DFLT, 
+			       new ConfigurableComponentPropertyAdapter() {
+				 public void propertyValueChanged(PropertyEvent e) {
+				 }
+			       });
+    propType.setToolTip(PROP_TYPE_DESC);
+
+    propIntensity = addProperty(PROP_INTENSITY, PROP_INTENSITY_DFLT, 
+			       new ConfigurableComponentPropertyAdapter() {
+				 public void propertyValueChanged(PropertyEvent e) {
+				 }
+			       });
+    propIntensity.setToolTip(PROP_INTENSITY_DESC);
+
+    propDuration = addProperty(PROP_DURATION, PROP_DURATION_DFLT, 
+			       new ConfigurableComponentPropertyAdapter() {
+				 public void propertyValueChanged(PropertyEvent e) {
+				 }
+			       });
+    propDuration.setToolTip(PROP_DURATION_DESC);
+  }
+
+  public StringBuffer getXML() {
+    // This should eventually use Xerces to construct itself.
+    StringBuffer sb = new StringBuffer();
+
+    sb.append("   <KINETIC LATITUDE=\"");
+    sb.append(getProperty(PROP_LATITUDE).getValue());
+    sb.append("\" LONGITUDE=\"");
+    sb.append(getProperty(PROP_LONGITUDE).getValue());
+    sb.append("\" TIME=\"");
+    sb.append(getProperty(PROP_TIME).getValue());
+    sb.append("\" TYPE=\"");
+    sb.append(getProperty(PROP_TYPE).getValue());
+    sb.append("\">\n");
+    sb.append("      <PARAM NAME=\"Intensity\" VALUE=\"");
+    sb.append(getProperty(PROP_INTENSITY).getValue());
+    sb.append("\"></PARAM>\n");
+    sb.append("      <PARAM NAME=\"Duration\" VALUE=\"");
+    sb.append(getProperty(PROP_DURATION).getValue());
+    sb.append("\"></PARAM>\n");
+    sb.append("   </CYBER>\n");
+
+    return sb;
+  }
+
+  /**
+   * Called when a new property has been added to the
+   * society. 
+   *
+   * @param PropertyEvent Event for the new property
+   */
+  public void propertyAdded(PropertyEvent e) {
+    Property addedProperty = e.getProperty();
+    Property myProperty = getProperty(addedProperty.getName().last().toString());
+    if (myProperty != null) {
+      setPropertyVisible(addedProperty, true);
+    }
+  }
+  /**
+   * Called when a property has been removed from the society
+   */
+  public void propertyRemoved(PropertyEvent e) {}
+
+}
---
+++ csmart/src/org/cougaar/tools/csmart/recipe/RecipeComponent.java
@@ -0,0 +1,53 @@
+/*
+ * <copyright>
+ *  Copyright 2000-2001 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.tools.csmart.ui.component;
+
+import java.net.URL;
+
+import org.cougaar.tools.csmart.ui.viewer.Organizer;
+import java.net.URL;
+
+/**
+ * Parent interface for all Recipes to a CSMART system.
+ * Recipes may have Agents, and may need to be able to write
+ * out information in the various Nodes. These interfaces are
+ * exposed.
+ */
+public interface RecipeComponent extends ModifiableComponent, ComponentProperties {
+
+  final String CLASS_NAME_PROP = "recipe_class_name";
+  final String RECIPE_QUERY_FILE= "recipeQueries.q";
+
+  void setName(String newName);
+
+  String getRecipeName();
+
+  /**
+   * Get the agents, both assigned and unassigned.
+   * @return array of agent components
+   */
+  AgentComponent[] getAgents();
+}
+
+
+
+
