3feb317522f2ccd8f13b51bdde07d857bf0d9ca7
ahelsing <ahelsing>
2002-01-29 23:40:37 +0000

    Change indents update copyright Support handing it a String to display, not just a URL

--- csmart/src/org/cougaar/tools/csmart/ui/Browser.java
+++ csmart/src/org/cougaar/tools/csmart/ui/Browser.java
@@ -1,6 +1,6 @@
 /* 
  * <copyright>
- *  Copyright 2001 BBNT Solutions, LLC
+ *  Copyright 2001,2002 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
  * 
  *  This program is free software; you can redistribute it and/or modify
@@ -31,93 +31,107 @@ import javax.swing.text.*;
 import javax.swing.text.html.*;
 
 public class Browser extends JFrame implements HyperlinkListener {
-    private static final Dimension DEFAULT_SIZE = new Dimension(600, 800);
-    private static final String BROWSER_TITLE = "CSMART Help Browser";
-    private static Browser singleton;
+  private static final Dimension DEFAULT_SIZE = new Dimension(600, 800);
+  private static final String BROWSER_TITLE = "CSMART Help Browser";
+  private static Browser singleton;
 
-    private JTextPane text = new JTextPane() {
-        protected void scrollToReference(String ref) {
-            super.scrollToReference(ref);
-            selectReference(ref);
-        }
+  private JTextPane text = new JTextPane() {
+      protected void scrollToReference(String ref) {
+	super.scrollToReference(ref);
+	selectReference(ref);
+      }
     };
-    private JScrollPane scroll = new JScrollPane();
+  private JScrollPane scroll = new JScrollPane();
 
-    private Browser() {
-        super(BROWSER_TITLE);
-        scroll.setViewport(new JViewport() {
-            public void scrollRectToVisible(Rectangle r) {
-                Dimension extent = getExtentSize();
-                r = new Rectangle(r.x, r.y, r.width, extent.height);
-                super.scrollRectToVisible(r);
-            }
-        });
-        scroll.setViewportView(text);
-        getContentPane().add(scroll);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                dispose();
-            }
-        });
-        text.setEditable(false);
-        scroll.setPreferredSize(DEFAULT_SIZE);
-        pack();
-        show();
-    }
+  private Browser() {
+    super(BROWSER_TITLE);
+    scroll.setViewport(new JViewport() {
+	public void scrollRectToVisible(Rectangle r) {
+	  Dimension extent = getExtentSize();
+	  r = new Rectangle(r.x, r.y, r.width, extent.height);
+	  super.scrollRectToVisible(r);
+	}
+      });
+    scroll.setViewportView(text);
+    getContentPane().add(scroll);
+    addWindowListener(new WindowAdapter() {
+	public void windowClosing(WindowEvent e) {
+	  dispose();
+	}
+      });
+    text.setEditable(false);
+    scroll.setPreferredSize(DEFAULT_SIZE);
+    pack();
+    show();
+  }
 
-    public void dispose() {
-        clearSingleton();
-        super.dispose();
-    }
+  public void dispose() {
+    clearSingleton();
+    super.dispose();
+  }
 
-    private HTMLDocument.Iterator getIterator(HTML.Tag tag) {
-        Document doc = text.getDocument();
-        if (doc instanceof HTMLDocument) {
-            HTMLDocument html = (HTMLDocument) doc;
-            return html.getIterator(tag);
-        }
-        return null;
+  private HTMLDocument.Iterator getIterator(HTML.Tag tag) {
+    Document doc = text.getDocument();
+    if (doc instanceof HTMLDocument) {
+      HTMLDocument html = (HTMLDocument) doc;
+      return html.getIterator(tag);
     }
+    return null;
+  }
 
-    private void selectReference(String ref) {
-        HTMLDocument.Iterator i = getIterator(HTML.Tag.A);
-        if (i != null) {
-            for (; i.isValid(); i.next()) {
-                AttributeSet a = i.getAttributes();
-                String nm = (String) a.getAttribute(HTML.Attribute.NAME);
-                if ((nm != null) && nm.equals(ref)) {
-                    // found a matching reference in the document.
-                    text.select(i.getStartOffset(), i.getEndOffset());
-                }
-            }
-        }
+  private void selectReference(String ref) {
+    HTMLDocument.Iterator i = getIterator(HTML.Tag.A);
+    if (i != null) {
+      for (; i.isValid(); i.next()) {
+	AttributeSet a = i.getAttributes();
+	String nm = (String) a.getAttribute(HTML.Attribute.NAME);
+	if ((nm != null) && nm.equals(ref)) {
+	  // found a matching reference in the document.
+	  text.select(i.getStartOffset(), i.getEndOffset());
+	}
+      }
     }
+  }
 
-    public void setURL(URL url) {
-        try {
-            text.setPage(url);
-            toFront();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+  public void setURL(URL url) {
+    try {
+      text.setPage(url);
+      toFront();
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
     }
+  }
 
-    public void hyperlinkUpdate(HyperlinkEvent e) {
-        if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-            setURL(e.getURL());
-        }
-    }
+  public void setText(String data) {
+    text.setText(data);
+    toFront();
+  }
 
-    private synchronized static void clearSingleton() {
-        singleton = null;
+  public void hyperlinkUpdate(HyperlinkEvent e) {
+    if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+      setURL(e.getURL());
     }
+  }
 
-    public synchronized static void setPage(URL url) {
-        if (singleton == null) {
-            singleton = new Browser();
-        } else {
-            singleton.setState(JFrame.NORMAL);
-        }
-        singleton.setURL(url);
+  private synchronized static void clearSingleton() {
+    singleton = null;
+  }
+
+  public synchronized static void setPage(URL url) {
+    if (singleton == null) {
+      singleton = new Browser();
+    } else {
+      singleton.setState(JFrame.NORMAL);
     }
+    singleton.setURL(url);
+  }
+
+  public synchronized static void setPage(String text) {
+    if (singleton == null) {
+      singleton = new Browser();
+    } else {
+      singleton.setState(JFrame.NORMAL);
+    }
+    singleton.setText(text);
+  }
 }
