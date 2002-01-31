b7d699f7759e88d9f2db6e72780a15732456e0fe
ahelsing <ahelsing>
2002-01-31 20:07:29 +0000

    Update copyright

--- csmart/src/org/cougaar/tools/csmart/experiment/ExperimentHost.java
+++ csmart/src/org/cougaar/tools/csmart/experiment/ExperimentHost.java
@@ -1,6 +1,6 @@
 /*
  * <copyright>
- *  Copyright 2000-2001 BBNT Solutions, LLC
+ *  Copyright 2000-2002 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
  * 
  *  This program is free software; you can redistribute it and/or modify
@@ -38,7 +38,6 @@ import java.io.IOException;
  * Maintains information about a host and generates the
  * host .ini file.
  **/
-
 public class ExperimentHost
   extends ModifiableConfigurableComponent
   implements Serializable, HostComponent
