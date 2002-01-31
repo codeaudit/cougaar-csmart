b1c91248fce3ae46ab02aa893a73f5fb33ce0a8a
ahelsing <ahelsing>
2002-01-31 20:16:15 +0000

    Fairly substantial modifications:
    The /data/common/servlets.txt file now expects the class for the
    loader of the servlet, as well as for the servlet itself.
    
    As a result, we can now use this file to handle servlets that are
    their own loader, as with the /load servlet.
    
    Added use of the logger
    Allow comment lines and blank lines in servlets.txt
    
    Note that although this is called ServletGroupInsertion, it is really
    PluginGroupInsertion -- to make it really be this would mostly just
    require cosmetic changes. I bet it could be used for that as is.

--- csmart/src/org/cougaar/tools/csmart/recipe/ServletGroupInsertionRecipe.java
+++ csmart/src/org/cougaar/tools/csmart/recipe/ServletGroupInsertionRecipe.java
@@ -1,6 +1,6 @@
 /* 
  * <copyright>
- *  Copyright 2001 BBNT Solutions, LLC
+ *  Copyright 2002 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
  * 
  *  This program is free software; you can redsitribute it and/or modify
@@ -20,32 +20,41 @@
  */ 
 package org.cougaar.tools.csmart.recipe;
 
-import java.io.Serializable;
 import java.sql.SQLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Vector;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.io.Serializable;
 import java.io.RandomAccessFile;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.StringTokenizer;
+
+import org.cougaar.util.ConfigFinder;
+import org.cougaar.util.log.Logger;
 
 import org.cougaar.tools.csmart.ui.util.Util;
 import org.cougaar.tools.csmart.core.property.Property;
 import org.cougaar.tools.csmart.core.property.ConfigurableComponentPropertyAdapter;
 import org.cougaar.tools.csmart.core.property.PropertyEvent;
+import org.cougaar.tools.csmart.core.property.range.StringRange;
 import org.cougaar.tools.csmart.core.cdata.ComponentData;
 import org.cougaar.tools.csmart.core.cdata.GenericComponentData;
-import org.cougaar.tools.csmart.core.property.range.StringRange;
-import org.cougaar.util.ConfigFinder;
 import org.cougaar.tools.csmart.core.db.PopulateDb;
 
 import org.cougaar.tools.csmart.society.AgentComponent;
 
+/**
+ * Recipe to easily select a set of servlets for inclusion in a society
+ * It gets a basic set of recipes from csmart/data/common/servlets.txt
+ * See that file for format.
+ * Users can use this recipe to add any other servlet they like
+ */
 public class ServletGroupInsertionRecipe extends RecipeBase
   implements Serializable
 {
@@ -67,15 +76,19 @@ public class ServletGroupInsertionRecipe extends RecipeBase
 
   private static final String PROP_NEW_SERVLETS_COUNT = "_Number of New Servlets";
   private static final Integer PROP_NEW_SERVLETS_COUNT_DFLT = new Integer(0);
-  private static final String PROP_NEW_SERVLETS_COUNT_DESC = "Number of servlets to be added that are not listed below.";
+  private static final Integer PROP_NEW_SERVLETS_ARG_COUNT_DFLT = new Integer(2);
+  private static final String PROP_NEW_SERVLETS_COUNT_DESC = "Number of servlets to be added that are not listed above.";
+
+  private static final String DFLT_SERVLET_CLASS = "org.cougaar.core.servlet.SimpleServletComponent";
 
 
   private Property[] propServlets;
   private Vector servletList = null;
   private Property propTargetAgentQuery;
   private Property propNewServletCount;
-  private Property[] propNewServlets = null;
-  private Property[] propNewServletArgs = null;
+  private Property[] propNewServlets = null; // class of servlet
+  private Property[] propNewServletNumArgs = null; // how many args it has
+  private Property[][] propNewServletArgs = null; // args for each new servlet
 
 
   public ServletGroupInsertionRecipe() {
@@ -125,23 +138,75 @@ public class ServletGroupInsertionRecipe extends RecipeBase
     // Annoying for the user, but it works.
     if( propNewServlets != null && count != propNewServlets.length ) {
       for(int i=0; i < propNewServlets.length; i++) {
-        removeProperty(propNewServlets[i]);
-        removeProperty(propNewServletArgs[i]);
+        removeProperty(propNewServlets[i]); // class
+	// Now the arguments
+	int numargs = 0;
+	if (propNewServletNumArgs[i] != null) {
+	  Integer na = (Integer)propNewServletNumArgs[i].getValue();
+	  numargs = na.intValue();
+	}
+	for (int j = 0; j < numargs; j++) {
+	  removeProperty(propNewServletArgs[i][j]);
+	}
+	// Now the number of arguments
+        removeProperty(propNewServletNumArgs[i]);
       }
     }
 
     propNewServlets = new Property[count];
-    propNewServletArgs = new Property[count];
+    propNewServletNumArgs = new Property[count];
+    propNewServletArgs = new Property[count][0];
 
     for(int i=0; i < count; i++) {
-      propNewServlets[i] = addProperty("Classname of servlet" + (i+1), "");
-      ((Property) propNewServlets[i]).setToolTip("Full classname of the new servlet." + (i+1));
+      propNewServlets[i] = addProperty("Classname of servlet " + (i+1), DFLT_SERVLET_CLASS);
+      ((Property) propNewServlets[i]).setToolTip("Full classname of the new servlet (loader) #" + (i+1));
 
-      propNewServletArgs[i] = addProperty("Path of servlet" + (i+1), "");
-      ((Property)propNewServletArgs[i]).setToolTip("Path to servlet." + (i+1));
-   }      
+      propNewServletNumArgs[i] = addProperty("Number of args for servlet " + (i+1), PROP_NEW_SERVLETS_ARG_COUNT_DFLT);
+      ((Property)propNewServletNumArgs[i]).setToolTip("Number of arguments for servlet " + (i+1));
+      ((Property)propNewServletNumArgs[i]).addPropertyListener(new ConfigurableComponentPropertyAdapter() {
+	  public void propertyValueChanged(PropertyEvent e) {
+	    String name = e.getProperty().getName().toString();
+	    String servnum = name.substring(name.indexOf("servlet") + 8);
+	    int snum = -1;
+	    try {
+	      snum = Integer.parseInt(servnum) - 1;
+	    } catch (NumberFormatException nfe) {}
+	    if (snum != -1) {
+	      updateNewServletArgCount(snum, (Integer)e.getProperty().getValue());
+	    } else {
+	      if (log.isErrorEnabled()) {
+		log.error("Couldn't figure out servlet being edited. name was " + name + ", servnum was " + servnum);
+	      }
+	    }
+	  }
+	});
+      updateNewServletArgCount(i, PROP_NEW_SERVLETS_ARG_COUNT_DFLT);
+    }
   }
 
+  // Update the arguments for a new servlet
+  private void updateNewServletArgCount(int servnum, Integer newCount) {
+    int count = newCount.intValue();
+    // First remove the old ones
+    if (propNewServletArgs != null && propNewServletArgs[servnum] != null && propNewServletArgs[servnum].length != count) {
+      for (int i = 0; i < (propNewServletArgs[servnum]).length; i++) {
+	removeProperty(propNewServletArgs[servnum][i]);
+      }
+    }
+
+    // Now add the new set in
+    propNewServletArgs[servnum] = new Property[count];
+    // The first argument is usually the real class of the servlet
+    if (count > 0) {
+      propNewServletArgs[servnum][0] = addProperty("New Servlet " + (servnum+1) + " arg 1", "");
+      ((Property) propNewServletArgs[servnum][0]).setToolTip("New Servlet " + (servnum+1) + " First argument, usu class");
+    }
+    for (int i = 1; i < count; i++) {
+      propNewServletArgs[servnum][i] = addProperty("New Servlet " + (servnum+1) + " arg " + (i+1), "");
+      ((Property) propNewServletArgs[servnum][i]).setToolTip("New Servlet " + (servnum+1) + " argument " + (i+1));
+    }
+  }
+  
   /**
    * Gets the name of the html help file for this component.
    *
@@ -163,8 +228,9 @@ public class ServletGroupInsertionRecipe extends RecipeBase
     try {
       inputFile = cf.locateFile("servlets.txt");
     } catch(Exception e) {
-      System.err.println("Could not read servlets file.");
-      e.printStackTrace();
+      if (log.isErrorEnabled()) {
+	log.error("Could not read servlets file.", e);
+      }
     } 
     if (vec == null) {
       try { 
@@ -173,20 +239,23 @@ public class ServletGroupInsertionRecipe extends RecipeBase
         // read servlets, one per line
         servletFile = new RandomAccessFile(inputFile, "r");      
         while (true) {
-          String isrv = servletFile.readLine(); // get servlet line      
+          String isrv = servletFile.readLine(); // get servlet line
+	  // Skip comment lines
           if (isrv == null) {
             break;
           }
           isrv = isrv.trim();
+	  if (isrv.startsWith("#") || isrv.equals(""))
+	    continue;
           ServletListRecord servletRecord = parseServletLine(isrv);
           vec.add(servletRecord);
         }
         servletFile.close();
 
       } catch (IOException e) {
-        e.printStackTrace();
-        System.err.println("Error during read/open from file: " + inputFile + 
-                           " " + e.toString()); 
+	if (log.isErrorEnabled()) {
+	  log.error("Error during read/open from file: " + inputFile, e);
+	}
       }
     }
     return vec;    
@@ -197,12 +266,16 @@ public class ServletGroupInsertionRecipe extends RecipeBase
     StringTokenizer tokens = new StringTokenizer(line, ",");
     String name = tokens.nextToken();
     String classname = tokens.nextToken();
-    String argument = tokens.nextToken();
     name = name.trim();
     classname = classname.trim();
-    argument = argument.trim();
+    ArrayList args = new ArrayList();
+    while (tokens.hasMoreTokens()) {
+      args.add(tokens.nextToken().trim());
+    }
+    String[] arguments = new String[args.size()];
+    arguments = (String[]) args.toArray(arguments);
 
-    ServletListRecord servRecord =  new ServletListRecord(name, classname, argument);
+    ServletListRecord servRecord =  new ServletListRecord(name, classname, arguments);
     return servRecord;
   }
   
@@ -211,7 +284,9 @@ public class ServletGroupInsertionRecipe extends RecipeBase
       Set targets = pdb.executeQuery(propTargetAgentQuery.getValue().toString());
       modifyComponentData(data, pdb, targets);
     } catch (SQLException sqle) {
-      sqle.printStackTrace();
+      if (log.isErrorEnabled()) {
+	log.error("Cant run agent target query", sqle);
+      }
     }
     return data;
   }
@@ -225,16 +300,25 @@ public class ServletGroupInsertionRecipe extends RecipeBase
      
       if (propServlets != null) {
         for(int i=0; i < propServlets.length; i++) {
+	  ServletListRecord servlet = (ServletListRecord)servletList.elementAt(i);
           if (((Property)propServlets[i]).getValue().toString().equals(TRUE)) {
             GenericComponentData plugin = new GenericComponentData();
             plugin.setType(ComponentData.PLUGIN);
-            plugin.setName("SimpleServletComponent" +(i+1));
-            plugin.setClassName("org.cougaar.core.servlet.SimpleServletComponent");
+
+	    String[] arguments = servlet.getArguments();
+	    for (int j = 0; j < arguments.length; j++) 
+	      plugin.addParameter(arguments[j]);
+
+	    // Component name is the class of the servlet plus its first
+	    // argument, or a number if none
+	    if (arguments.length > 0) {
+	      plugin.setName(servlet.getClassname().substring(servlet.getClassname().lastIndexOf('.')+1) + "-" + arguments[0]);
+	    } else {
+	      plugin.setName(servlet.getClassname() + (i+1));
+	    }
+            plugin.setClassName(servlet.getClassname());
             plugin.setParent(data);
             plugin.setOwner(this);
- 
-            plugin.addParameter(((ServletListRecord)servletList.elementAt(i)).getClassname());
-            plugin.addParameter(((ServletListRecord)servletList.elementAt(i)).getArgument());
             data.addChildDefaultLoc(plugin);
           }
         }
@@ -243,13 +327,23 @@ public class ServletGroupInsertionRecipe extends RecipeBase
         for(int i=0; i < propNewServlets.length; i++) { 
             GenericComponentData comp = new GenericComponentData();
             comp.setType(ComponentData.PLUGIN);
-            comp.setName("SimpleServletComponent" +(i+1));
-            comp.setClassName("org.cougaar.core.servlet.SimpleServletComponent");
+	    String newcls = (String)propNewServlets[i].getValue();
+	    // How many args does the servlet have
+	    int numargs = ((Integer)propNewServletNumArgs[i].getValue()).intValue();
+	    // Component name is the class of the servlet plus its first
+	    // argument, or a number if none
+	    if (numargs > 0) {
+	      comp.setName(newcls.substring(newcls.lastIndexOf('.')+1) + "-" +  ((String)propNewServletArgs[i][0].getValue()));
+	    } else {
+	      comp.setName(newcls +(i+1));
+	    }
+            comp.setClassName(newcls);
             comp.setParent(data);
             comp.setOwner(this);
- 
-            comp.addParameter((String)propNewServlets[i].getValue());
-            comp.addParameter((String)propNewServletArgs[i].getValue());
+
+	    // For each arg it has, write it out
+	    for (int j = 0; j < numargs; j++)
+	      comp.addParameter((String)propNewServletArgs[i][j].getValue());
             data.addChildDefaultLoc(comp);
         }
       }
@@ -268,11 +362,11 @@ public class ServletGroupInsertionRecipe extends RecipeBase
 
     private String name;
     private String classname;
-    private String argument;
+    private String[] argument;
 
     public ServletListRecord() {}
     
-    public ServletListRecord(String name, String classname, String argument) {
+    public ServletListRecord(String name, String classname, String argument[]) {
       this.name = name;
       this.classname = classname;
       this.argument = argument;
@@ -284,11 +378,16 @@ public class ServletGroupInsertionRecipe extends RecipeBase
     public String getClassname() {
       return classname;
     }
-    public String getArgument() {
+    public String[] getArguments() {
       return argument;
     }
     public String toString() {
-      return (name + " " +classname + " " +argument);
+      StringBuffer buf = new StringBuffer();
+      buf.append(name + " " +classname);
+      for (int i = 0; i < argument.length; i++) {
+	buf.append(" " +argument[i]);
+      }
+      return buf.toString();
     }
   }
 }
