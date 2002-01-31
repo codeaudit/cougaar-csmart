a18ddce4329d902228401895ffb3c28a2c011759
bkrisler <bkrisler>
2002-01-31 20:55:57 +0000

    Added two methods:  getPluginNames()  getPluginArgs(String name)

--- csmart/src/org/cougaar/tools/csmart/core/cdata/AgentComponentData.java
+++ csmart/src/org/cougaar/tools/csmart/core/cdata/AgentComponentData.java
@@ -21,6 +21,7 @@
 package org.cougaar.tools.csmart.core.cdata;
 
 import org.cougaar.tools.csmart.core.property.ConfigurableComponent;
+import java.util.ArrayList;
 
 /**
  * AgentComponentData extends ComponentData and fills in the
@@ -35,4 +36,48 @@ public class AgentComponentData extends GenericComponentData {
     type = ComponentData.AGENT;
     className = defClassName;
   }
+
+  /**
+   * Returns a list of all plugin names for this agent.
+   *
+   * @return a <code>String[]</code> value
+   */
+  String[] getPluginNames() {
+    ArrayList names = new ArrayList();
+    ComponentData[] plugins = getChildren();
+    
+    for(int i=0; i < plugins.length; i++) {
+      ComponentData plugin = plugins[i];
+      if(plugin.getType().equals(ComponentData.PLUGIN)) {
+        names.add(plugin.getName());
+      }
+    }
+    
+    return (String[])names.toArray(new String[names.size()]);
+  }
+  
+  /**
+   * Returns all arg for a specific plugin
+   *
+   * @param name Name of the plugin
+   * @return an <code>Object[]</code> value
+   */
+  Object[] getPluginArgs(String name) {
+    ComponentData plugin = findPlugin(name);
+    if(plugin != null) {
+      return plugin.getParameters();
+    }
+    return null;
+  }
+
+  private ComponentData findPlugin(String name) {
+    ComponentData[] plugins = getChildren();
+    for(int i=0; i < plugins.length; i++) {
+      if(plugins[i].getName().equals(name)) {
+        return plugins[i];
+      }
+    }
+    return null;
+  }
+
 }
