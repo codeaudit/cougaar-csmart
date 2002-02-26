955f885244c1002f0913c531e85c9317778e70e4
ahelsing <ahelsing>
2002-02-26 22:36:41 +0000

    Cleanup Don't bother specifying org.cougaar.class.path -- it appears un-neccessary

--- csmart/bin/Monitor.bat
+++ csmart/bin/Monitor.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM "<copyright>"
-REM " Copyright 2001 BBNT Solutions, LLC"
+REM " Copyright 2001,2002 BBNT Solutions, LLC"
 REM " under sponsorship of the Defense Advanced Research Projects Agency (DARPA)."
 REM ""
 REM " This program is free software; you can redistribute it and/or modify"
@@ -27,24 +27,11 @@ CALL %COUGAAR_INSTALL_PATH%\bin\setlibpath.bat
 
 REM Use the Bootstrapper to find Jar files.
 
-REM Monitor required Jars are listed here for convenience.
-
-REM add csmart.jar
-rem SET LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\csmart.jar
-
-REM for CSMART environment, use the following
-rem SET LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\server.jar
-
-REM Plus these third party jar files, which are in CIP\sys
-rem SET LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\xerces.jar
-rem SET LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\jcchart451K.jar
-rem SET LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\grappa1_2.jar
-
 SET MYMEMORY=-Xms100m -Xmx300m
 SET MYPROPERTIES=-Dorg.cougaar.install.path=%COUGAAR_INSTALL_PATH%
 SET MYCONFIGPATH=-Dorg.cougaar.config.path="%COUGAAR_INSTALL_PATH%/csmart/data/common/\;"
 
 @ECHO ON
 
-java.exe %MYPROPERTIES% %MYMEMORY% %MYCONFIGPATH% -Dorg.cougaar.class.path=%LIBPATHS% -classpath %LIBPATHS% org.cougaar.core.node.Bootstrapper org.cougaar.tools.csmart.ui.monitor.viewer.CSMARTUL
+java.exe %MYPROPERTIES% %MYMEMORY% %MYCONFIGPATH% -classpath %LIBPATHS% org.cougaar.core.node.Bootstrapper org.cougaar.tools.csmart.ui.monitor.viewer.CSMARTUL
 
