d1577c33074c2a9e10b42596ee960a9aa27ebb88
ahelsing <ahelsing>
2002-04-21 19:31:54 +0000

    Use new AgentBase Add some code that notionally handles binders. FIXME: This needs testing!

--- csmart/src/org/cougaar/tools/csmart/society/file/AgentFileComponent.java
+++ csmart/src/org/cougaar/tools/csmart/society/file/AgentFileComponent.java
@@ -36,26 +36,17 @@ import org.cougaar.tools.csmart.society.AgentComponent;
 import org.cougaar.tools.csmart.society.AssetComponent;
 import org.cougaar.tools.csmart.society.ContainerBase;
 import org.cougaar.tools.csmart.society.PluginBase;
+import org.cougaar.tools.csmart.society.AgentBase;
+import org.cougaar.tools.csmart.society.BinderBase;
 import org.cougaar.tools.csmart.ui.viewer.CSMART;
 import org.cougaar.util.log.Logger;
 
 public class AgentFileComponent
-  extends ModifiableConfigurableComponent
+  extends AgentBase
   implements AgentComponent {
 
   private transient Logger log;
-  private String name;
-  private String classname;
   private String filename;
-
-  /** Agent Classname Property Definitions **/
-
-  /** Classname Property Definition **/
-  public static final String PROP_CLASSNAME = "Classname";
-
-  /** Classname Description Property Definition **/
-  public static final String PROP_CLASSNAME_DESC = "Name of the Agent Class";
-  
   /**
    * Creates a new <code>AgentFileComponent</code> instance.
    *
@@ -77,64 +68,14 @@ public class AgentFileComponent
     log = CSMART.createLogger(this.getClass().getName());
   }
 
-  /**
-   * Initialize all local properties
-   *
-   */
-  public void initProperties() {
-    Property p = addProperty(PROP_CLASSNAME, new String(classname));
-    p.setToolTip(PROP_CLASSNAME_DESC);
-
-    addPlugins();
-    addAssetData();
-  }
-
-  /**
-   * Adds any relevent <code>ComponentData</code> for this component.
-   * This method does not modify any existing <code>ComponentData</code>
-   *
-   * @see ComponentData
-   * @param data Pointer to the global <code>ComponentData</code>
-   * @return an updated <code>ComponentData</code> object
-   */
-  public ComponentData addComponentData(ComponentData data) {
-    // Process AssetData
-    Iterator iter = 
-      ((Collection)getDescendentsOfClass(AssetComponent.class)).iterator();
-    while(iter.hasNext()) {
-      AssetComponent asset = (AssetComponent)iter.next();
-      asset.addComponentData(data);
-    }
-
-    // Process Plugins
-    iter = 
-      ((Collection)getDescendentsOfClass(ContainerBase.class)).iterator();
-    while(iter.hasNext()) {
-      ContainerBase container = (ContainerBase)iter.next();
-      if(container.getShortName().equals("Plugins")) {
-        for(int i=0; i < container.getChildCount(); i++) {
-          GenericComponentData plugin = null;
-          PluginBase pg = (PluginBase) container.getChild(i);
-          plugin = new GenericComponentData();
-          plugin.setType(ComponentData.PLUGIN);
-          plugin.setParent(data);
-          plugin.setOwner(this);
-          data.addChild(pg.addComponentData(plugin));
-        }
-      }
-    }
-   
-    return data;
-  }
-
-  private void addAssetData() {
+  protected void addAssetData() {
     BaseComponent asset = 
       (BaseComponent)new AssetFileComponent(filename, getShortName());
     asset.initProperties();
     addChild(asset);
   }
 
-  private void addPlugins() {
+  protected void addPlugins() {
     ContainerBase container = new ContainerBase("Plugins");
     container.initProperties();
     addChild(container);
@@ -162,29 +103,34 @@ public class AgentFileComponent
     }
   }
 
-  /**
-   * Tests equality for agents.
-   * Agents are equal if they have the same short name.
-   * @param o Object to test for equality 
-   * @return returns true if the object is an AgentComponent with same name
-   */
-  public boolean equals(Object o) {
-    if (o instanceof AgentComponent) {
-      AgentComponent that = (AgentComponent)o;
-      if (!this.getShortName().equals(that.getShortName())  ) {
-	return false;
-      }     
-      return true;
+  protected void addBinders() {
+    ContainerBase container = new ContainerBase("Binders");
+    container.initProperties();
+    addChild(container);
+
+    if(log.isDebugEnabled()) {
+      log.debug("Parse: " + filename);
     }
-    return false;
+    ComponentDescription[] desc = 
+      ComponentConnector.parseFile(filename);
+    for (int i=0; i < desc.length; i++) {
+      if(log.isDebugEnabled()) {
+        log.debug("Create Binder: " + desc[i].getName());
+      }
+      String name = desc[i].getName();
+      int index = name.lastIndexOf('.');
+      if (index != -1)
+        name = name.substring(index+1);
+      BinderBase binder = 
+        new BinderBase(name, desc[i].getClassname());
+      binder.initProperties();
+
+      // FIXME: Must I change ComponentConnector in some way here?
+      Iterator iter = ComponentConnector.getPluginProps(desc[i]);
+      while(iter.hasNext()) 
+        binder.addParameter((String)iter.next());
+      container.addChild(binder);
+    }
   }
 
-  private void readObject(ObjectInputStream ois)
-    throws IOException, ClassNotFoundException
-  {
-    ois.defaultReadObject();
-    createLogger();
-  }
-
-
-}
+} // end of AgentFileComponent
