6886f69bfeb289215bd47ad94c28d2d0df98b458
bkrisler <bkrisler>
2002-07-02 18:37:10 +0000

    Added initFromDb and initFromCData methods.

--- csmart/src/org/cougaar/tools/csmart/recipe/ComplexRecipeBase.java
+++ csmart/src/org/cougaar/tools/csmart/recipe/ComplexRecipeBase.java
@@ -21,13 +21,20 @@
 package org.cougaar.tools.csmart.recipe;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import org.cougaar.core.agent.ClusterImpl;
 import org.cougaar.tools.csmart.core.cdata.AgentComponentData;
 import org.cougaar.tools.csmart.core.cdata.ComponentData;
 import org.cougaar.tools.csmart.core.cdata.GenericComponentData;
+import org.cougaar.tools.csmart.core.db.DBUtils;
 import org.cougaar.tools.csmart.core.db.PDbBase;
 import org.cougaar.tools.csmart.core.db.PopulateDb;
 import org.cougaar.tools.csmart.core.property.ConfigurableComponent;
@@ -36,8 +43,9 @@ import org.cougaar.tools.csmart.core.property.ModificationEvent;
 import org.cougaar.tools.csmart.core.property.Property;
 import org.cougaar.tools.csmart.society.AgentComponent;
 import org.cougaar.tools.csmart.society.SocietyComponentCreator;
+import org.cougaar.tools.csmart.society.cdata.AgentCDataComponent;
+import org.cougaar.tools.csmart.society.db.AgentDBComponent;
 import org.cougaar.tools.csmart.ui.viewer.GUIUtils;
-import org.cougaar.tools.csmart.recipe.cdata.ComplexRecipeCDataComponent;
 
 /**
  * ComplexRecipeBase.java
@@ -64,10 +72,17 @@ public class ComplexRecipeBase extends RecipeBase
   /** Identifier Tag for a ComplexRecipe **/
   public static final String RECIPE_CLASS = "##RECIPE_CLASS##";
 
+  private static final String QUERY_AGENT_NAMES = "queryAgentNames";
+
+  public static final String PROP_TARGET_COMPONENT_QUERY = "Target Component Selection Query";
+  public static final String PROP_TARGET_COMPONENT_QUERY_DFLT = "recipeQuerySelectNothing";
+  public static final String PROP_TARGET_COMPONENT_QUERY_DESC = 
+    "The query name for selecting components to modify";
+
   protected String assemblyId = null;
   protected String oldAssemblyId = null;
   private boolean saveInProgress = false;
-
+  protected ComponentData cdata = null;
   private Property propAssemblyId;
 
 
@@ -77,6 +92,12 @@ public class ComplexRecipeBase extends RecipeBase
   
   public ComplexRecipeBase (String name, String assemblyId){
     super(name);
+    this.assemblyId = assemblyId;
+  }
+
+  public ComplexRecipeBase (ComponentData cdata, String assemblyId) {
+    super(cdata.getName());
+    this.cdata = cdata;
     this.assemblyId = assemblyId;
   }
 
@@ -152,6 +173,18 @@ public class ComplexRecipeBase extends RecipeBase
     return ret;
   }
 
+  public void addTargetQueryProperty() {
+    Property p = addRecipeQueryProperty(PROP_TARGET_COMPONENT_QUERY, PROP_TARGET_COMPONENT_QUERY_DFLT);
+    p.setToolTip(PROP_TARGET_COMPONENT_QUERY_DESC);
+  }
+
+  protected Property addRecipeQueryProperty(String name, String dflt) {
+    Property prop = addProperty(new RecipeQueryProperty(this, name, dflt));
+    prop.setPropertyClass(String.class);
+    return prop;
+  }
+
+
   /**
    * Initializes the hidden assembly id property.
    *
@@ -159,8 +192,90 @@ public class ComplexRecipeBase extends RecipeBase
   public void initProperties() {
     propAssemblyId = addProperty(ASSEMBLY_PROP, ((assemblyId != null) ? assemblyId : ""));
     propAssemblyId.setVisible(false);
-    
+
+    if(assemblyId != null) {
+      if(cdata != null)
+        initFromCData();
+      else 
+        initFromDatabase();
+    } else if (cdata != null) {
+      initFromCData();
+    }
+
   }
+
+  private void initFromDatabase() {
+    Map substitutions = new HashMap();
+    if (assemblyId != null) {
+      substitutions.put(":assemblyMatch", DBUtils.getListMatch(assemblyId));
+      substitutions.put(":insertion_point", "Node.AgentManager.Agent");
+
+      // FIXME:
+      // It would be really nice to be able to handle Binders and other non-Agent
+      // top-level things
+
+      try {
+	Connection conn = DBUtils.getConnection();
+	try {
+	  Statement stmt = conn.createStatement();
+	  String query = DBUtils.getQuery(QUERY_AGENT_NAMES, substitutions);
+	  ResultSet rs = stmt.executeQuery(query);
+	  while (rs.next()) {
+	    String agentName = DBUtils.getNonNullString(rs, 1, query);
+	    AgentDBComponent agent = 
+              new AgentDBComponent(agentName, assemblyId);
+	    agent.initProperties();
+	    addChild(agent);
+	  }
+	  rs.close();
+	  stmt.close();
+	} finally {
+	  conn.close();
+	}
+      } catch (Exception e) {
+        if(log.isErrorEnabled()) {
+          log.error("Exception", e);
+        }
+	throw new RuntimeException("Error" + e);
+      }
+      // After reading the soc from the DB, it is not modified.
+      //      modified = false;
+      modified = false;
+      fireModification(new ModificationEvent(this, RECIPE_SAVED));
+    }    
+  }
+
+  private void initFromCData() {
+    // create society properties from cdata
+    // create agents from cdata
+    ArrayList agentData = new ArrayList();
+    ArrayList alldata = new ArrayList();
+    if (cdata != null)
+      alldata.add(cdata);
+    
+    // FIXME: It'd be nice to deal with binders of Agents in here!!!
+
+    // Find all the agents
+    for (int i = 0; i < alldata.size(); i++) {
+      ComponentData someData = (ComponentData)alldata.get(i);
+      if (someData.getType().equals(ComponentData.AGENT)) {
+        agentData.add(someData);
+      } else {
+        ComponentData[] moreData = someData.getChildren();
+        for (int j = 0; j < moreData.length; j++) 
+          alldata.add(moreData[j]);
+      }
+    }
+
+    // For each agent, create a component and add it as a child
+    for (int i = 0; i < agentData.size(); i++) {
+      AgentComponent agentComponent = 
+        new AgentCDataComponent((ComponentData)agentData.get(i));
+      agentComponent.initProperties();
+      addChild(agentComponent);
+    }
+  }
+
 
   /**
    * Get the assembly id for this Recipe.
@@ -248,7 +363,7 @@ public class ComplexRecipeBase extends RecipeBase
     
     ComponentData cdata = getComponentData();
     cdata.setName(name);
-    RecipeComponent component = new ComplexRecipeCDataComponent(cdata, null);
+    RecipeComponent component = new ComplexRecipeBase(cdata, null);
     component.initProperties();
 
     ((ComplexRecipeBase)component).modified = this.modified;
