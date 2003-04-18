6e933528c0168efb4a79e742f92cff52c0117bc0
bkrisler <bkrisler>
2003-04-18 19:11:26 +0000

    Cleaned up loaders.

--- csmart/src/org/cougaar/tools/csmart/util/XMLUtils.java
+++ csmart/src/org/cougaar/tools/csmart/util/XMLUtils.java
@@ -2,11 +2,11 @@
  * <copyright>
  *  Copyright 2002-2003 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the Cougaar Open Source License as published by
  *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
- * 
+ *
  *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
  *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
  *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
@@ -36,6 +36,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 
 /**
  * XMLUtils.java
@@ -48,10 +49,9 @@ import java.io.PrintWriter;
  * @version 1.0
  */
 public class XMLUtils {
-  private Logger log;
+  private static Logger log = CSMART.createLogger("org.coguaar.tools.csmart.util.XMLUtils");
 
   public XMLUtils() {
-    log = CSMART.createLogger("org.cougaar.tools.csmart.util.XMLUtils");
   }
 
   /**
@@ -60,47 +60,28 @@ public class XMLUtils {
    * @param filename Name of file to load.
    * @return a <code>Document</code> value, null on error or nonexistent file
    */
-  public Document loadXMLFile(String filename) {
+  public static Document loadXMLFile(String filename) {
     if (filename == null || filename.equals(""))
       return null;
 
+    // Try to open the file in an input stream. Note it may not exist!
+    InputStream is = null;
     try {
-      DOMParser parser = new DOMParser();
-
-      // Try to open the file in an input stream. Note it may not exist!
-      InputStream is = null;
-      try {
-	is = ConfigFinder.getInstance("csmart").open(filename);
-      } catch (IOException ioe) {
-	if (log.isWarnEnabled()) {
-	  log.warn("Could not open " + filename + " for reading: " + ioe);
-	}
-	return null;
+      is = ConfigFinder.getInstance("csmart").open(filename);
+    } catch (IOException ioe) {
+      if (log.isWarnEnabled()) {
+        log.warn("Could not open " + filename + " for reading: " + ioe);
       }
-      if (is == null) {
-	if (log.isWarnEnabled()) {
-	  log.warn("Could not open " + filename + " for reading.");
-	}
-	return null;
+      return null;
+    }
+    if (is == null) {
+      if (log.isWarnEnabled()) {
+        log.warn("Could not open " + filename + " for reading.");
       }
-
-      parser.parse(new InputSource(is));
-      return parser.getDocument();
-    } catch (org.xml.sax.SAXParseException spe) {
-      if (log.isErrorEnabled()) {
-	log.error("Parse exception Parsing file: " + filename, spe);
-      }
-    } catch (org.xml.sax.SAXException se) {
-      if (log.isErrorEnabled()) {
-	log.error("SAX exception Parsing file: " + filename, se);
-      }
-    } catch (Exception e) {
-      if (log.isErrorEnabled()) {
-	log.error("Exception Parsing file: " + filename, e);
-      }
+      return null;
     }
 
-    return null;
+    return loadXMLFile(is, filename);
   }
 
   /**
@@ -109,25 +90,29 @@ public class XMLUtils {
    * @param file - Handle to xml file.
    * @return a <code>Document</code> value, null on error
    */
-  public Document loadXMLFile(File file) {
-    if (file == null || ! file.canRead() || file.isDirectory())
+  public static Document loadXMLFile(File file) throws FileNotFoundException {
+    if (file == null || !file.canRead() || file.isDirectory())
       return null;
 
+    return loadXMLFile(new FileInputStream(file), file.getName());
+  }
+
+  private static Document loadXMLFile(InputStream stream, String name) {
     try {
       DOMParser parser = new DOMParser();
-      parser.parse(new InputSource(new FileInputStream(file)));
+      parser.parse(new InputSource(stream));
       return parser.getDocument();
     } catch (org.xml.sax.SAXParseException spe) {
       if (log.isErrorEnabled()) {
-	log.error("Parse exception Parsing file: " + file.getName(), spe);
+        log.error("Parse exception Parsing file: " + name, spe);
       }
     } catch (org.xml.sax.SAXException se) {
       if (log.isErrorEnabled()) {
-	log.error("SAX exception Parsing file: " + file.getName(), se);
+        log.error("SAX exception Parsing file: " + name, se);
       }
     } catch (Exception e) {
       if (log.isErrorEnabled()) {
-	log.error("Exception Parsing file: " + file.getName(), e);
+        log.error("Exception Parsing file: " + name, e);
       }
     }
 
@@ -136,7 +121,7 @@ public class XMLUtils {
 
   /**
    * Writes the contents of the <code>Node</code> to the specified
-   * file, in XML format. 
+   * file, in XML format.
    *
    * @param configDir - Directory to write new xml file.
    * @param node - Document Node to dump to xml file.
@@ -144,96 +129,92 @@ public class XMLUtils {
    * @see org.w3c.dom.Node
    * @exception IOException if an error occurs
    */
-  public void writeXMLFile(File configDir, Node node, String name) 
-    throws IOException {
-    
+  public static void writeXMLFile(File configDir, Node node, String name)
+      throws IOException {
+
     Logger log = CSMART.createLogger("org.cougaar.tools.csmart.util.XMLUtils");
 
-    if(!name.endsWith(".xml")) {
+    if (!name.endsWith(".xml")) {
       name = name + ".xml";
     }
     PrintWriter writer = new PrintWriter(new FileWriter(new File(configDir, name)));
     try {
       writeNode(writer, node, 0);
     } catch (Exception e) {
-      if(log.isErrorEnabled()) {
+      if (log.isErrorEnabled()) {
         log.error("Error writing config file: " + e);
       }
-    }
-    finally {
+    } finally {
       writer.close();
-    }    
+    }
 
   }
-  
-  private void writeNode(PrintWriter writer, Node node, int indent) {
+
+  private static void writeNode(PrintWriter writer, Node node, int indent) {
     StringBuffer ibuff = new StringBuffer();
-    for(int i=0; i < indent; i++) {
+    for (int i = 0; i < indent; i++) {
       ibuff.append(" ");
     }
     int type = node.getNodeType();
-    switch(type) {
-    case Node.DOCUMENT_NODE:
-        writer.println("<?xml version=\"1.0\" encoding=\""+
-                           "UTF-8" + "\"?>");  
+    switch (type) {
+      case Node.DOCUMENT_NODE:
+        writer.println("<?xml version=\"1.0\" encoding=\"" +
+                       "UTF-8" + "\"?>");
         indent = -2;
-        break;                  
-    case Node.ELEMENT_NODE:
-      writer.print(ibuff.substring(0) + '<' + node.getNodeName() );
-      NamedNodeMap nnm = node.getAttributes();
-      if(nnm != null )
-        {
-          int len = nnm.getLength() ;
+        break;
+      case Node.ELEMENT_NODE:
+        writer.print(ibuff.substring(0) + '<' + node.getNodeName());
+        NamedNodeMap nnm = node.getAttributes();
+        if (nnm != null) {
+          int len = nnm.getLength();
           Attr attr;
-          for ( int i = 0; i < len; i++ )
-            {
-              attr = (Attr)nnm.item(i);
-              writer.print(' ' 
-                               + attr.getNodeName()
-                               + "=\""
-                               + attr.getNodeValue()
-                               +  '"' );
-            }
+          for (int i = 0; i < len; i++) {
+            attr = (Attr) nnm.item(i);
+            writer.print(' '
+                         + attr.getNodeName()
+                         + "=\""
+                         + attr.getNodeValue()
+                         + '"');
+          }
         }
-      writer.println('>');
-      break;
-      
+        writer.println('>');
+        break;
+
       case Node.ENTITY_REFERENCE_NODE:
-        writer.print('&' + node.getNodeName() + ';' );
+        writer.print('&' + node.getNodeName() + ';');
         break;
       case Node.CDATA_SECTION_NODE:
-        writer.print( "<![CDATA[" 
-                          + node.getNodeValue()
-                          + "]]>" );
-        break;       
+        writer.print("<![CDATA["
+                     + node.getNodeValue()
+                     + "]]>");
+        break;
       case Node.TEXT_NODE:
         writer.print(ibuff.substring(0) + node.getNodeValue());
         break;
       case Node.PROCESSING_INSTRUCTION_NODE:
-        writer.print(ibuff.substring(0) + "<?" 
-                         + node.getNodeName() ) ;
+        writer.print(ibuff.substring(0) + "<?"
+                     + node.getNodeName());
         String data = node.getNodeValue();
-        if ( data != null && data.length() > 0 ) {
+        if (data != null && data.length() > 0) {
           writer.print(' ');
           writer.print(data);
         }
         writer.println("?>");
         break;
-      
+
     }//end of switch
-    
-    
+
+
     //recurse
-    for(Node child = node.getFirstChild(); child != null; 
-        child = child.getNextSibling()) {
-      writeNode(writer, child, indent+2);
-      }
-    
+    for (Node child = node.getFirstChild(); child != null;
+         child = child.getNextSibling()) {
+      writeNode(writer, child, indent + 2);
+    }
+
     //without this the ending tags will miss
-    if ( type == Node.ELEMENT_NODE )
-      {
-        writer.println(ibuff.substring(0) + "</" + node.getNodeName() + ">");
-      }
+    if (type == Node.ELEMENT_NODE) {
+      writer.println(ibuff.substring(0) + "</" + node.getNodeName() + ">");
+    }
   }
 
 }// XMLUtils
