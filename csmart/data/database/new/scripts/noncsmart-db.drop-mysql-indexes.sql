7e43d27fbdb07f635cef7df0d70abd3a4993121e
ahelsing <ahelsing>
2003-04-23 22:05:27 +0000

    use new ref-csv

--- csmart/data/database/new/scripts/noncsmart-db.drop-mysql-indexes.sql
+++
@@ -1,8 +0,0 @@
-DROP INDEX ref_org_list37 ON org_pg_attr;
-DROP INDEX ref_org_list38 ON org_pg_attr;
-DROP INDEX reforg_rel31 ON org_relation;
-DROP INDEX reforg_rel32 ON org_relation;
-DROP INDEX reflib_org45 ON lib_organization;
-DROP INDEX reflib_org46 ON lib_organization;
-DROP INDEX refoplan20 ON oplan_agent_attr;
-DROP INDEX refoplan21 ON oplan_agent_attr;
--- csmart/data/database/new/scripts/noncsmart-db.drop-mysql-tables.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS lib_organization;
-DROP TABLE IF EXISTS lib_pg_attribute;
-DROP TABLE IF EXISTS org_relation;
-DROP TABLE IF EXISTS org_pg_attr;
-DROP TABLE IF EXISTS lib_oplan;
-DROP TABLE IF EXISTS alploc;
-DROP TABLE IF EXISTS oplan_agent_attr;
