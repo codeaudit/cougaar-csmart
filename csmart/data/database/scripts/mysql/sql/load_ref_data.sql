df2de7b45f227425576d3ac6fb3987dcbf638b7c
ahelsing <ahelsing>
2003-04-29 20:17:47 +0000

    Make sure column names capitalized consistently and use varchar over char consistently -- make refconfigdb and configuration db identical

--- csmart/data/database/scripts/mysql/sql/load_ref_data.sql
+++ csmart/data/database/scripts/mysql/sql/load_ref_data.sql
@@ -4,11 +4,12 @@
 
 DROP TABLE IF EXISTS alploc;
 CREATE TABLE alploc (
-  alploc_code char(50) binary NOT NULL default '',
-  location_name char(50) binary default NULL,
-  latitude decimal(68,30) default NULL,
-  longitude decimal(68,30) default NULL,
-  installation_type_code char(3) binary default NULL
+  ALPLOC_CODE VARCHAR(50) BINARY NOT NULL DEFAULT '',
+  LOCATION_NAME VARCHAR(50) BINARY DEFAULT NULL,
+  LATITUDE DECIMAL(68,30) DEFAULT NULL,
+  LONGITUDE DECIMAL(68,30) DEFAULT NULL,
+  INSTALLATION_TYPE_CODE CHAR(3) BINARY DEFAULT NULL,
+  UNIQUE KEY pk_alploc (ALPLOC_CODE)
 ) TYPE=MyISAM;
 
 LOAD DATA INFILE ':cip/csmart/data/database/ref-csv/alploc.csv.tmp'
@@ -281,10 +282,10 @@ LOAD DATA INFILE ':cip/csmart/data/database/ref-csv/geoloc.csv.tmp'
 
 DROP TABLE IF EXISTS lib_organization;
 CREATE TABLE lib_organization (
-  ORG_ID varchar(50) binary NOT NULL default '',
-  ORG_NAME varchar(50) binary default NULL,
-  UIC varchar(50) binary default NULL,
-  ORG_CLASS varchar(50) binary default NULL,
+  ORG_ID VARCHAR(50) BINARY NOT NULL DEFAULT '',
+  ORG_NAME VARCHAR(50) BINARY DEFAULT NULL,
+  UIC VARCHAR(50) BINARY DEFAULT NULL,
+  ORG_CLASS VARCHAR(50) BINARY DEFAULT NULL,
   UNIQUE KEY pk_lib_organization (ORG_ID),
   KEY reflib_org45 (ORG_NAME),
   KEY reflib_org46 (ORG_ID)
@@ -305,11 +306,11 @@ LOAD DATA INFILE ':cip/csmart/data/database/ref-csv/lib_organization.csv.tmp'
 
 DROP TABLE IF EXISTS lib_pg_attribute;
 CREATE TABLE lib_pg_attribute (
-  PG_ATTRIBUTE_LIB_ID varchar(100) binary NOT NULL default '',
-  PG_NAME varchar(50) binary default NULL,
-  ATTRIBUTE_NAME varchar(50) binary default NULL,
-  ATTRIBUTE_TYPE varchar(50) binary default NULL,
-  AGGREGATE_TYPE varchar(50) binary default NULL,
+  PG_ATTRIBUTE_LIB_ID VARCHAR(100) BINARY NOT NULL DEFAULT '',
+  PG_NAME VARCHAR(50) BINARY DEFAULT NULL,
+  ATTRIBUTE_NAME VARCHAR(50) BINARY DEFAULT NULL,
+  ATTRIBUTE_TYPE VARCHAR(50) BINARY DEFAULT NULL,
+  AGGREGATE_TYPE VARCHAR(50) BINARY DEFAULT NULL,
   UNIQUE KEY pk_lib_pg_attribute (PG_ATTRIBUTE_LIB_ID)
 ) TYPE=MyISAM;
 
@@ -330,12 +331,13 @@ LOAD DATA INFILE ':cip/csmart/data/database/ref-csv/lib_pg_attribute.csv.tmp'
 DROP TABLE IF EXISTS lib_oplan;
 DROP TABLE IF EXISTS oplan;
 CREATE TABLE oplan (
-  oplan_id char(50) binary NOT NULL default '',
-  operation_name char(50) binary default NULL,
-  priority char(50) binary default NULL,
-  min_planning_offset    decimal(68,30)       default NULL,
-  start_offset    decimal(68,30)         default NULL,
-  end_offset    decimal(68,30)           default NULL
+  OPLAN_ID VARCHAR(50) BINARY NOT NULL DEFAULT '',
+  OPERATION_NAME VARCHAR(50) BINARY DEFAULT NULL,
+  PRIORITY VARCHAR(50) BINARY DEFAULT NULL,
+  MIN_PLANNING_OFFSET    DECIMAL(68,30)       DEFAULT NULL,
+  START_OFFSET    DECIMAL(68,30)         DEFAULT NULL,
+  END_OFFSET    DECIMAL(68,30)           DEFAULT NULL,
+  UNIQUE KEY pk_oplan (OPLAN_ID)
 ) TYPE=MyISAM;
 
 LOAD DATA INFILE ':cip/csmart/data/database/ref-csv/oplan.csv.tmp'
@@ -352,13 +354,14 @@ LOAD DATA INFILE ':cip/csmart/data/database/ref-csv/oplan.csv.tmp'
 
 DROP TABLE IF EXISTS oplan_agent_attr;
 CREATE TABLE oplan_agent_attr (
-  oplan_id char(50) binary NOT NULL default '',
-  org_id char(150) binary NOT NULL default '',
-  component_id char(50) binary NOT NULL default '',
-  start_cday decimal(68,30) NOT NULL default '0.000000000000000000000000000000',
-  attribute_name char(50) binary NOT NULL default '',
-  end_cday decimal(68,30) default NULL,
-  attribute_value char(50) binary default NULL
+  OPLAN_ID VARCHAR(50) BINARY NOT NULL DEFAULT '',
+  ORG_ID varchar(150) BINARY NOT NULL DEFAULT '',
+  COMPONENT_ID VARCHAR(50) BINARY NOT NULL DEFAULT '',
+  START_CDAY DECIMAL(68,30) NOT NULL DEFAULT '0.000000000000000000000000000000',
+  ATTRIBUTE_NAME varchAR(50) BINARY NOT NULL DEFAULT '',
+  END_CDAY DECIMAL(68,30) DEFAULT NULL,
+  ATTRIBUTE_VALUE varCHAR(50) BINARY DEFAULT NULL,
+  UNIQUE KEY pk_oplan_agent_attr (OPLAN_ID, ORG_ID, COMPONENT_ID, START_CDAY, ATTRIBUTE_NAME)
 ) TYPE=MyISAM;
 
 LOAD DATA INFILE ':cip/csmart/data/database/ref-csv/oplan_agent_attr.csv.tmp'
@@ -375,12 +378,12 @@ LOAD DATA INFILE ':cip/csmart/data/database/ref-csv/oplan_agent_attr.csv.tmp'
 
 DROP TABLE IF EXISTS org_pg_attr;
 CREATE TABLE org_pg_attr (
-  ORG_ID varchar(50) binary NOT NULL default '',
-  PG_ATTRIBUTE_LIB_ID varchar(100) binary NOT NULL default '',
-  ATTRIBUTE_VALUE varchar(50) binary NOT NULL default '',
-  ATTRIBUTE_ORDER decimal(68,30) NOT NULL default '0.000000000000000000000000000000',
-  START_DATE datetime NOT NULL default '0000-00-00 00:00:00',
-  END_DATE datetime default NULL,
+  ORG_ID VARCHAR(50) BINARY NOT NULL DEFAULT '',
+  PG_ATTRIBUTE_LIB_ID VARCHAR(100) BINARY NOT NULL DEFAULT '',
+  ATTRIBUTE_VALUE VARCHAR(50) BINARY NOT NULL DEFAULT '',
+  ATTRIBUTE_ORDER DECIMAL(68,30) NOT NULL DEFAULT '0.000000000000000000000000000000',
+  START_DATE DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+  END_DATE DATETIME DEFAULT NULL,
   UNIQUE KEY pk_org_pg_attr (ORG_ID,PG_ATTRIBUTE_LIB_ID,ATTRIBUTE_VALUE,ATTRIBUTE_ORDER,START_DATE),
   KEY ref_org_list37 (ORG_ID),
   KEY ref_org_list38 (PG_ATTRIBUTE_LIB_ID)
@@ -401,11 +404,11 @@ LOAD DATA INFILE ':cip/csmart/data/database/ref-csv/org_pg_attr.csv.tmp'
 
 DROP TABLE IF EXISTS org_relation;
 CREATE TABLE org_relation (
-  ROLE varchar(50) binary NOT NULL default '',
-  SUPPORTING_ORG_ID varchar(150) binary NOT NULL default '',
-  SUPPORTED_ORG_ID varchar(150) binary NOT NULL default '',
-  START_DATE datetime NOT NULL default '0000-00-00 00:00:00',
-  END_DATE datetime default NULL,
+  ROLE VARCHAR(50) BINARY NOT NULL DEFAULT '',
+  SUPPORTING_ORG_ID VARCHAR(150) BINARY NOT NULL DEFAULT '',
+  SUPPORTED_ORG_ID VARCHAR(150) BINARY NOT NULL DEFAULT '',
+  START_DATE DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+  END_DATE DATETIME DEFAULT NULL,
   UNIQUE KEY pk_org_relation (ROLE,SUPPORTING_ORG_ID,SUPPORTED_ORG_ID,START_DATE),
   KEY reforg_rel31 (SUPPORTING_ORG_ID),
   KEY reforg_rel32 (SUPPORTED_ORG_ID)
