362a16352e23fd5ddc929970fd5597c5ac4be90b
mkappler <mkappler>
2004-03-24 20:02:46 +0000

    Adding a utility script that will rename the extended oplan tables to the names necessary to be loaded in the database by the load scripts.  Running with various arguments will toggle the files from their original compressed form to the extended form for the 3 csv files affected.

---
+++ csmart/data/database/scripts/mysql/toggle_ext_oplan.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# 
+# <copyright>
+#  Copyright 2001-2003 BBNT Solutions, LLC
+#  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+# 
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the Cougaar Open Source License as published by
+#  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+# 
+#  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+#  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+#  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+#  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+#  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+#  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+#  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+#  PERFORMANCE OF THE COUGAAR SOFTWARE.
+# </copyright>
+
+if [ "x$1" = "x" ]; then
+    echo "Usage: toggle_ext_oplan.sh [compressed] or [extended]"
+    echo "   "
+    echo "     Specify whether you want the csv files to be in the "
+    echo "            'compressed' or 'extended' oplan form. "
+    exit
+fi
+
+
+if [ "$1" = "extended" ]; then
+    mv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_agent_attr.csv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_agent_attr_orig.csv
+    mv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_agent_attr_ext.csv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_agent_attr.csv
+
+    mv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_stage.csv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_stage_orig.csv
+    mv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_stage_ext.csv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_stage.csv
+
+    mv $COUGAAR_INSTALL_PATH/fcsua/data/database/csv/mission_period_3a.csv $COUGAAR_INSTALL_PATH/fcsua/data/database/csv/mission_period_3a_orig.csv
+    mv $COUGAAR_INSTALL_PATH/fcsua/data/database/csv/mission_period_3a_ext.csv $COUGAAR_INSTALL_PATH/fcsua/data/database/csv/mission_period_3a.csv
+
+else
+    mv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_agent_attr.csv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_agent_attr_ext.csv
+    mv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_agent_attr_orig.csv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_agent_attr.csv
+
+    mv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_stage.csv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_stage_ext.csv
+    mv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_stage_orig.csv $COUGAAR_INSTALL_PATH/csmart/data/database/csv/oplan_stage.csv
+
+    mv $COUGAAR_INSTALL_PATH/fcsua/data/database/csv/mission_period_3a.csv $COUGAAR_INSTALL_PATH/fcsua/data/database/csv/mission_period_3a_ext.csv
+    mv $COUGAAR_INSTALL_PATH/fcsua/data/database/csv/mission_period_3a_orig.csv $COUGAAR_INSTALL_PATH/fcsua/data/database/csv/mission_period_3a.csv
+
+fi
